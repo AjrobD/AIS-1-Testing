cf4febab7a4b42bfcd8130bc9b8698188d7698f6
AjrobD <borja_dosuna@hotmail.es>
2021-03-07 00:45:53 +0100

    Add book not finished

--- b.dosuna.2017-j.gonzalezca.2017/src/test/java/es/urjc/code/daw/library/BookServiceTest.java
+++ b.dosuna.2017-j.gonzalezca.2017/src/test/java/es/urjc/code/daw/library/BookServiceTest.java
@@ -1,5 +1,34 @@
 package es.urjc.code.daw.library;
 
-public class BookServiceTest {
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.urjc.code.daw.library.book.Book;
+import es.urjc.code.daw.library.book.BookRepository;
+import es.urjc.code.daw.library.book.BookService;
+import es.urjc.code.daw.library.notification.NotificationService;
+
+
+public class BookServiceTest {
+	
+	@Test
+	public void givenBookService_whenABookIsAdded_thenUserIsAdded_and_thenNotificationIsSent() {
+		//given
+		BookRepository bookRepository = mock(BookRepository.class);
+		NotificationService notificationService = mock(NotificationService.class);
+		BookService bookService = new BookService(bookRepository,notificationService);
+		Logger logger = mock(Logger.class);
+		//when
+		Book book = mock(Book.class);
+		when(bookRepository.save(book)).thenReturn(book);
+		bookService.save(book);
+		//when(notificationService.notify("Book Event: book with title="+book.getTitle()+" was created")).then(logger.info("Book Event: book with title="+book.getTitle()+" was created"));
+		doAnswer(invocation -> {
+			Logger logger = LoggerFactory.getLogger(NotificationService.class);
+			logger.info("Book Event: book with title="+book.getTitle()+" was created");
+		}).when(notificationService).notify("Book Event: book with title="+book.getTitle()+" was created");
+	}
 }
