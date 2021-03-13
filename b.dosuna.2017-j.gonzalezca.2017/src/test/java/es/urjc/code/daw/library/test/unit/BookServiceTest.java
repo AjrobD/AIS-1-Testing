ffd74239cd070cb4a1f53e19766be7e4f98f6468
dejaimenpax <jaimegonzalezcasero@gmail.com>
2021-03-13 10:26:46 +0100

    Merged project with first exercise completed

--- b.dosuna.2017-j.gonzalezca.2017/src/test/java/es/urjc/code/daw/library/test/unit/BookServiceTest.java
+++ b.dosuna.2017-j.gonzalezca.2017/src/test/java/es/urjc/code/daw/library/test/unit/BookServiceTest.java
@@ -13,7 +13,7 @@ import es.urjc.code.daw.library.book.BookRepository;
 import es.urjc.code.daw.library.book.BookService;
 import es.urjc.code.daw.library.notification.NotificationService;
 
-
+@DisplayName("Tests unitarios de la logica de la aplicacion")
 public class BookServiceTest {
 	
 	BookService bookService;
@@ -31,6 +31,7 @@ public class BookServiceTest {
 	}
 	
 	@Test
+	@DisplayName("Comprobar que cuando se guarda un libro utilizando BookService, se guarda en el repositorio y se lanza una notificacion")
 	public void givenBookService_whenABookIsAdded_thenBookIsAddedToRepository_and_thenNotificationIsSent() {
 		//given: done in set up
 		//when
@@ -42,5 +43,23 @@ public class BookServiceTest {
 		verify(bookRepository).save(book);
 		verify(notificationService).notify("Book Event: book with title="+book.getTitle()+" was created");
 	}
-
+	
+	@Test
+	@DisplayName("Comprobar que cuando se borra un libro utilizando BookService, se elimina del repositorio y se lanza una notificacion")
+	public void givenBookService_whenABookIsDeleted_thenIsDeletedFromBookRepository_and_thenNotificationIsSent() {
+		Book book = mock(Book.class);
+		when(book.getTitle()).thenReturn("Prueba");
+		while(bookRepository.existsById(book.getId())) {
+			book.setId(book.getId()+1);
+		}
+		when(bookRepository.save(book)).thenReturn(book);
+		bookService.save(book);
+		bookService.delete(book.getId());
+		//delete method is called once
+		verify(bookRepository, times(1)).deleteById(book.getId());
+		//book is really deleted
+		when(bookRepository.existsById(book.getId())).thenReturn(false);
+		//API's logger shows correct message
+		verify(notificationService).notify("Book Event: book with id="+book.getId()+" was deleted");
+	}
 }
