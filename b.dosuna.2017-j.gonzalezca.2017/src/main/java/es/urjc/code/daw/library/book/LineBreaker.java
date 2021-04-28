d8b7de7e02fba7a8c8662b65ee03dd294f9f00c3
dejaimenpax <jaimegonzalezcasero@gmail.com>
2021-04-28 12:31:58 +0200

    Add another recursive method with another alternative
    Test with lineLength = 9 not working

--- b.dosuna.2017-j.gonzalezca.2017/src/main/java/es/urjc/code/daw/library/book/LineBreaker.java
+++ b.dosuna.2017-j.gonzalezca.2017/src/main/java/es/urjc/code/daw/library/book/LineBreaker.java
@@ -4,31 +4,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LineBreaker {
-	public static String breakText(String text, int lineLength) {
-		if(text.length()<=lineLength) {
-			return text;
+	
+	
+	private static String multipleChecker(String word, int givenLength) {
+		if (word.length() <= givenLength) {
+			return word;
 		}
 		else {
-			String aux = text.trim().replaceAll(" +", " ");
-			String[] lines = aux.split(" ");
-			String output = "";
-			int charCount = 0;
-			for(int i=0;i<lines.length;i++) {
-				charCount += lines[i].length();
-				if(charCount>lineLength) {
-					if(output.charAt(output.length()-1) == ' ') {
-						output = output.substring(0,output.length()-1);
-					}
-					output += "\n";
-					charCount=0;
-				}
-				output += lines[i];
-				if(i<lines.length-1)
-					output += " ";
-			}
-			return output;
+			String aux = word.substring(0, givenLength-1);
+			aux += "-\n";
+			return aux += multipleChecker( word.substring(givenLength-1, word.length()), givenLength) ;
 		}
 	}
+	
+	
+	public static String breakText(String text, int lineLength) {
+		
+		String aux = text.trim().replaceAll(" +", " ");
+		String[] lines = aux.split(" ");
+		String output = "";
+		int wordNumber = lines.length;
+		for(int i=0; i<wordNumber; i++) {
+			output += multipleChecker(lines[i], lineLength);
+			output += "\n";
+		}
+		return output.substring(0, output.length()-1);	
+	}
+	
+	
 }
 
 
