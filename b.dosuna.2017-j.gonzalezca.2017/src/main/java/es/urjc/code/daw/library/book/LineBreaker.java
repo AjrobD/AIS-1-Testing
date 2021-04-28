0c0ac8757f9017e1ae300f0c964eeee53f3b41ee
dejaimenpax <jaimegonzalezcasero@gmail.com>
2021-04-28 17:49:41 +0200

    Add a method which can be helpful
    The idea is using specialTreatment in order to check if two or more
    consecutive words fit into the same line, always if lineLength is bigger
    than all of these words added (including whitespaces)

--- b.dosuna.2017-j.gonzalezca.2017/src/main/java/es/urjc/code/daw/library/book/LineBreaker.java
+++ b.dosuna.2017-j.gonzalezca.2017/src/main/java/es/urjc/code/daw/library/book/LineBreaker.java
@@ -18,11 +18,23 @@ public class LineBreaker {
 	}
 	
 	
-	public static String breakText(String text, int lineLength) {
-		
-		String aux = text.trim().replaceAll(" +", " ");
-		String[] lines = aux.split(" ");
+	private static int specialTreatment(String[] wordsArray, int givenLength) {
 		String output = "";
+		for (int k=0; ( k < wordsArray.length-1 && output.length() < givenLength ); k++) {
+			output += wordsArray[k];
+			output += " ";
+		}
+		if (output.charAt(givenLength) == ' ') {
+			return ((output.length() - 1) / givenLength);
+		}
+		else return 0;
+	}
+	
+	
+	public static String breakText(String text, int lineLength) {
+		String aux = text.trim().replaceAll(" +", " ");
+		String output = "";
+		String[] lines = aux.split(" ");
 		int wordNumber = lines.length;
 		for(int i=0; i<wordNumber; i++) {
 			output += multipleChecker(lines[i], lineLength);
